2ee7614e45dc2b02ef8d206ee32c1d90883fe21a
andreiosg <andrej.gardijan@protonmail.com>
2023-08-17 08:10:38 +0200

    refactor(debug): avoid gcc non defined

--- include/bfci/debug.h
+++ include/bfci/debug.h
@@ -21,7 +21,7 @@
         if (TRACE_ON)                                                             \
             fprintf(stderr,                                                       \
                     "%s:%s:" ESTRINGIFY(__LINE__) ":(" STRINGIFY(X) "): " F "\n", \
-                    __FILE_NAME__,                                                \
+                    __FILE__,                                                \
                     __func__,                                                     \
                     (X));                                                         \
     } while (false)

add22f26feded5d8481160cb955a8846490b2e90
andreiosg <andrej.gardijan@protonmail.com>
2023-09-10 20:44:43 +0200

    test: add initial tests skeleton, help test

---
+++ tests/Tests.cmake
@@ -0,0 +1,15 @@
+include(CTest)
+
+enable_testing()
+
+set(TEST_HELP help)
+add_test(
+    NAME ${TEST_HELP}
+    COMMAND $<TARGET_FILE:${BFCI_APP_TARGET}>
+)
+
+set_tests_properties(
+    ${TEST_HELP}
+PROPERTIES
+    WILL_FAIL TRUE
+)

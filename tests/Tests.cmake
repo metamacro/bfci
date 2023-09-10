0c7ab78b7e9ea1d3508058d15941f9355033aaaa
andreiosg <andrej.gardijan@protonmail.com>
2023-09-11 00:52:04 +0200

    test(ctest): add tests for example brainfuck programs

--- tests/Tests.cmake
+++ tests/Tests.cmake
@@ -8,6 +8,16 @@ add_test(
     COMMAND $<TARGET_FILE:${BFCI_APP_TARGET}>
 )
 
+file(GLOB BF_PROGS "${CMAKE_SOURCE_DIR}/examples/*.bf")
+
+foreach(BF_PROG IN LISTS BF_PROGS)
+    get_filename_component(BF_PROG_NAME ${BF_PROG} NAME_WE)
+    add_test(
+        NAME ${BF_PROG_NAME}
+        COMMAND $<TARGET_FILE:${BFCI_APP_TARGET}> ${BF_PROG}
+    )
+endforeach()
+
 set_tests_properties(
     ${TEST_HELP}
     PROPERTIES

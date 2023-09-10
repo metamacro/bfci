148b80f888fa4ac50141622f52969c332c49495f
andreiosg <andrej.gardijan@protonmail.com>
2023-09-11 00:58:29 +0200

    test(ctest): add help, examples tests comments

--- tests/Tests.cmake
+++ tests/Tests.cmake
@@ -2,14 +2,20 @@ include(CTest)
 
 enable_testing()
 
+# test without args - prints help message and fails
 set(TEST_HELP help)
 add_test(
     NAME ${TEST_HELP}
     COMMAND $<TARGET_FILE:${BFCI_APP_TARGET}>
 )
+set_tests_properties(
+    ${TEST_HELP}
+    PROPERTIES
+    WILL_FAIL TRUE
+)
 
+# test each brainfuck program from examples
 file(GLOB BF_PROGS "${CMAKE_SOURCE_DIR}/examples/*.bf")
-
 foreach(BF_PROG IN LISTS BF_PROGS)
     get_filename_component(BF_PROG_NAME ${BF_PROG} NAME_WE)
     add_test(
@@ -17,9 +23,3 @@ foreach(BF_PROG IN LISTS BF_PROGS)
         COMMAND $<TARGET_FILE:${BFCI_APP_TARGET}> ${BF_PROG}
     )
 endforeach()
-
-set_tests_properties(
-    ${TEST_HELP}
-    PROPERTIES
-    WILL_FAIL TRUE
-)
